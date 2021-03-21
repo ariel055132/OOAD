2a690b862172edb5975bddcfb6856bf676f6e80e
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2021-03-21 16:47:08 +0800

    String data size is added

--- Pointers/Chapter1/datatypeSize.cpp
+++ Pointers/Chapter1/datatypeSize.cpp
@@ -8,9 +8,12 @@ int main() {
     double c;
     long d;
     char e;
+    string f;
     cout << sizeof(a) << endl; // (int) 4 bytes
     cout << sizeof(b) << endl; // (float) 4 bytes
     cout << sizeof(c) << endl; // (double) 8 bytess
     cout << sizeof(d) << endl; // (long) 8 bytes
     cout << sizeof(e) << endl; // (char) 1 bytes
+    cout << sizeof(f) << endl; // (string) 32 bytes
+    return 0;
 }
